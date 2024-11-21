841d9eadf1009d20a6ee54b9c6a39f6a75f7c8a0
noora-q0 <128188552+noora-q0@users.noreply.github.com>
2024-11-21 11:27:38 -0500

    Update prison_dashboard.py

--- prison_dashboard.py
+++ prison_dashboard.py
@@ -2252,7 +2252,7 @@ import os
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8050))  # Default to 8050 if PORT is not set
-    app.run_server(host="0.0.0.0", port=port, debug=True)
+    app.run_server(host="0.0.0.0", port=port, debug=False)
 
 
 
